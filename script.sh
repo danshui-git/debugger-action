b90389e80073d26544cac8eedac583f95f33fbf2
danshui-git <68673669+danshui-git@users.noreply.github.com>
2021-03-23 21:50:48 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -127,8 +127,7 @@ echo -e "$TIMEOUT_MESSAGE"
 if [[ -n "$TELEGRAM_BOT_TOKEN" ]]; then
   MSG="SSH: ${SSH_LINE}\nWEB: ${WEB_LINE}"
   echo -n "Sending information to Telegram Bot......"
-  curl -k --data chat_id="${TELEGRAM_CHAT_ID}" --data "text=${SSH_LINE}
-  web ${WEB_LINE}" "https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage"
+  curl -k --data chat_id="${TELEGRAM_CHAT_ID}" --data "text=${SSH_LINE}\nweb ${WEB_LINE}" "https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage"
   echo ""
 fi
 
